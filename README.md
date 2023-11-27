b32dbdcfe8808290b9b25a223bcdb6a095576ef3
94Seyli <ilyes.boukaffal+github@gmail.com>
2023-11-27 18:13:54 +0100

    Réinitialisation branche Part 1

--- README.md
+++
@@ -1,9 +0,0 @@
-Name of project team members : 
-- Ilyes Boukaffal
-- Pedro Alves (test)
-
-Inscription for use : 
-
-any dependencies to be installed :
-
-list of know bugs :
--- main.py
+++
@@ -1,4 +0,0 @@
-from os import listdir
-from os.path import isfile,join
-files = [f for f in listdir('speeches-20231123') if isfile(join('speeches-20231123', f))]
-print (files)
