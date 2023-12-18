7a5feaf81590c76ccfabf7ee2cf5ad105d475716
94Seyli <ilyes.boukaffal+github@gmail.com>
2023-12-18 22:02:21 +0100

    maj

--- function.py
+++ function.py
@@ -1,1 +1,462 @@
+# _________________________Modules_________________________#
+import os
+import random
+import math
+import tkinter as tk
+from tkinter import messagebox, scrolledtext, PhotoImage, Canvas
 
+
+# _________________________Basic functions_________________________#
+# function to run through the list of files with a given extension and in a given directory and return a list of names
+def list_of_files(directory, extension):
+    files_names = []
+    for filename in os.listdir(directory):
+        if filename.endswith(extension):
+            files_names.append(filename)
+    return files_names
+
+
+# function to associate first and last name of presidents
+def associate_name_presidents(president_name):
+    president_f_name = {
+        "Giscard dEstaing": "Valéry",
+        "Mitterand": "François",
+        "Chirac": "Jacques",
+        "Sarkozy": "Nicolas",
+        "Hollande": "François",
+        "Macron": "Emmanuel",
+    }
+    return president_f_name.get(president_name, " Unknow ")
+
+
+# function which takes the list of files and extracts only the name of the presidents and stored them in a list
+def new_list(l):
+    list = []
+    for i in range(len(l)):
+        val = l[i][11:-4]  # removes the file format (-4th index) and "nomination_" (11th index)
+        for j in range(len(val)):
+            if val[j] == '1' or val[j] == '2':  # if there is a 1 or a 2 in the string
+                val = val[:-1]  # removes them from the last position
+        list.append(val)
+    return list
+
+
+# function for removing duplicates from a list and return the list without duplicates
+def del_duplicates(l):
+    l = list(set(l))  # set allows to delete automatically duplicates
+    return l
+
+
+# function to convert a text/string to lower cases and return the text in lower case
+def lower_case(txt):
+    txt = list(txt)
+    for i in range(len(txt)):
+        if ord(txt[i]) >= ord("A") and ord(txt[i]) <= ord("Z"):  # if a character is between ASCII code of A and Z
+            txt[i] = chr(ord(txt[i]) + ord("a") - ord("A"))  # change upper case to lower case
+    return "".join(txt)
+
+
+# function that removes punctuation from a string
+def punctuation_str(l):
+    list_punctuation = ["!", "+", "#", ".", ",", "?", ";", ":", "(", ")", ":", "=", "`", '"']
+    list_s_punctuation = ["'", "-"]
+    list = []
+    string = ""
+    for i in range(len(l)):
+        list.append(l[i])
+        if list[i] in list_s_punctuation:
+            list[i] = " "
+        if list[i] in list_punctuation:
+            list[i] = ""
+        string = string + list[i]
+    return string
+
+
+# function to cleans files and adds them to cleaned depository
+def cleaned(files_list):
+    if os.path.exists("./cleaned"):  # if cleaned directory exist, delete it automatically
+        for file in os.listdir("./cleaned"):
+            path_file = os.path.join("./cleaned", file)
+            os.remove(path_file)
+        os.rmdir("./cleaned")
+
+    os.mkdir('./cleaned/')  # Create Cleaned directory
+
+    for file_name in files_list:
+        path_in = os.path.join('./speeches-20231123', file_name)
+        path_out = os.path.join('./cleaned', "cleaned_" + file_name)
+        with open(path_in, "r", encoding="utf-8") as f:
+            content = f.read()
+
+        # Use punctuation and lower case functions
+        cleaned_content = punctuation_str(lower_case(content))
+
+        with open(path_out, 'w', encoding='utf-8') as p:
+            p.write(cleaned_content)
+
+
+# _______________________________TF-IDF_______________________________#
+
+# function to scan line of string and return a collection with the words of strings
+def scan_line(string):
+    coll = {}
+    word = ""
+    string = string + " "
+
+    for i in range(len(string)):
+        if string[i] != " ":
+            word = word + string[i]
+        else:
+            if word == "d" or word == "j" or word == "s" or word == "n":  # otherwise add an e after the letters d, j, s, n
+                word = word + "e"
+            if word == "c":  # if it's a c, add an ela
+                word = word + "ela"
+            if word == "l":  # if it's an l, there's a 50/50 chance that it's an a or an e
+                a_or_e = random.randint(1, 2)
+                if a_or_e == 1:
+                    word = word + "e"
+                else:
+                    word = word + "a"
+                if word in coll:  # if the content of the variable word is in the collection, add 1
+                    coll[word] = coll[word] + 1
+                else:
+                    coll[word] = 1
+            else:
+                if word in coll:  # if the content of the variable word is in the collection, add 1
+                    coll[word] = coll[word] + 1
+                else:
+                    coll[word] = 1
+            word = ""
+    return coll
+
+
+# function to calcul iteration of word and put it in a collection
+def cpt_word(directory):
+    collection = {}
+    text = ""
+    name_list = list_of_files(directory, "txt")
+    for i in range(len(name_list)):
+        loc_file = "./" + str(directory) + "/" + name_list[i]
+        with open(loc_file, "r") as f:
+            line = f.readline()
+            while line != "":
+                line = line.replace("\n", "")
+                text = text + line + " "
+                line = f.readline()
+        collection = scan_line(text)
+    return collection
+
+
+# function to compute the IDF score of a word and put it in a collection
+def IDF_score_coll(directory):
+    TF = {}
+    coll = {}
+    name_list = list_of_files(directory, "txt")
+    for i in range(len(name_list)):
+        loc_file = "/" + name_list[i]
+        with open(directory + loc_file, 'r') as f:
+            content_ = f.read()
+            content_ = content_.replace("\n", " ")
+            TF = scan_line(content_)
+        for word in TF.keys():  # Browse keys to the TF collection, i.e. all the words in text
+            if word not in coll.keys():
+                coll[word] = 1  # If the word is not in the IDF collection, then value = 1
+            else:
+                coll[word] = coll[word] + 1  # If the word is in the IDF collection, then value = +1
+    for word, count in coll.items():
+        coll[word] = float(math.log((len(name_list) / count), 10))  # Formula of IDF score
+    return coll
+
+
+# function that takes a directory and returns TF-IDF matrix based on each queue
+def TFIDF_matrix(directory):
+    coll_TFIDF = {}
+    coll_TF = {}
+    List = []
+
+    name_list = list_of_files(directory, "txt")
+    name_list = sorted(name_list)
+    List.append("word")
+
+    coll_IDF = IDF_score_coll(directory)  # Calculation of the IDF score for each word in the directory
+    all_word = cpt_word(directory)  # Counts the number of occurrences of each word in all the documents
+
+    if "" in all_word:  # Deletes the empty key if there is one
+        del all_word[""]
+
+    matrix = []
+    line_numb = int(len(name_list)) + 1  # number of lines in the matrix (number of words + 1 for titles)
+    for l in range(len(all_word) + 1):  # Creates a matrix filled with zeros
+        line = [0] * line_numb
+        matrix.append(line)
+    cpt = 1
+
+    for word in all_word.keys():  # Fill the first column of the matrix with the words
+        matrix[cpt][0] = word
+        cpt += 1
+    for i in range(len(name_list)):  # Loop over the queues to fill the matrix with TF-IDF values
+
+        loc_file = "./" + str(directory) + "/" + name_list[i]  # Follows the path of the file
+        with open(loc_file, "r") as f:
+            lineTF = f.read()
+            lineTF = lineTF.replace("\n", "")
+            coll_TF = scan_line(lineTF)  # Calculation of TF-IDF values for each word in the file
+            for words in coll_TF.keys():
+                if words in coll_IDF:
+                    coll_TFIDF[words] = coll_IDF[words] * coll_TF[words]
+            coll_TF.clear()
+
+            matrix[0][i + 1] = name_list[i]  # Filling the first line of the matrix with file names
+
+            for h in range(len(matrix)):  # Filling the matrix with TF-IDF values
+
+                for word in coll_TFIDF.keys():
+                    if word == matrix[h][0]:
+                        matrix[h][i + 1] = coll_TFIDF[word]
+            coll_TFIDF.clear()
+    return matrix
+
+
+# ----------------------------------
+'''
+       Input function for a number between 0 and 9.
+
+       No input arguments.
+
+       Output:
+       - value: int
+           Numeric value between 0 and 9 (a single character entered).
+
+       The function uses a loop to ensure correct and valid input.
+       If the input contains more than one character, the function ignores the input and continues the loop.
+       If the input is a number between 0 and 5, the function returns this value as an integer.
+       '''
+
+
+def saisie():
+    flag = True
+    entry = ""
+    while flag:
+        entry = input("Enter a number between 0 and 5 : ")
+        if len(entry) > 1:
+            break
+        elif ord(entry) > 47 and ord(entry) < 54:  # Entry is a number between 0 and 9
+            return int(entry)
+
+
+def word_question(string):
+    list = []
+    word = ""
+    string = string + " "
+    for i in range(len(string)):
+        if string[i] != " ":
+            word = word + string[i]
+        else:
+            if word == "d" or word == "j" or word == "s" or word == "n":  # otherwise add an e after the letters d, j, s, n
+                word = word + "e"
+            if word == "c":  # if it's a c, add an ela
+                word = word + "ela"
+            if word == "l":  # if it's an l, there's a 50/50 chance that it's an a or an e
+                a_or_e = random.randint(1, 2)
+                if a_or_e == 1:
+                    word = word + "e"
+                else:
+                    word = word + "a"
+            if word not in list:  # if the content of the variable word is in the collection, add 1
+                list.append(word)
+            word = ""
+    return list
+
+
+def important_word(list, matrix):
+    word_list = []
+    for word in list:
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if matrix[i][j] == word:
+                    word_list.append(word)
+    return word_list
+
+
+def cpt_word_question(coll):
+    cpt_total = 0
+    for number in coll.values():
+        cpt_total = cpt_total + number
+    return cpt_total
+
+
+def matrix_filtre_matrix(coll, matrix, coll_IDF):
+    matrix = []
+    line_0 = [0, "Question"]
+    matrix.append(line_0)
+    for word in coll.keys():
+        for i in range(len(matrix)):
+            if matrix[i][0] == word:
+                L = []
+                TF_IDF = coll[word] * coll_IDF[word]
+                L.append(word)
+                L.append(TF_IDF)
+                matrix.append(L)
+
+        print(matrix)
+    return matrix
+
+
+"""
+#Calculating similarity between two vectors
+def scale_product(A,B,column_A,column_B):
+    sum = 0
+    for i in range (1,len(A)):
+        sum = sum + A[i][column_A]*B[i][column_B]
+    return sum
+
+def vector_norm(A, column):
+    sum = 0
+    for i in range(1,len(A)):
+        sum = sum + A[i][column]**2
+    sum = sum**0.5
+    return sum
+
+
+    Calculates the similarity (cosine) between two vectors.
+    Args:
+    A, B (list): Two lists representing the vectors.
+    column_A, column_B (int): Column indexs to be used in vectors A and B.
+    Returns:
+    float: Cosine similarity between vectors A and B.
+def similarity_calculation(A, B, column_A, column_B):
+    norms_product = vector_norm(A, column_A) * vector_norm(B, column_B)
+    if norms_product == 0:
+        return 0  #Avoid division by zero
+    return scale_product(A, B, column_A, column_B) / norms_product
+
+def similarity(matrix_question_M, matrix_corpus_M):
+    collection_similarity_vector = {}
+    for i in range(1, len(matrix_corpus_M[0])):
+        similarity_score = similarity_calculation(matrix_question_M, matrix_corpus_M, 1, i)
+        collection_similarity_vector[matrix_corpus_M[0][i]] = similarity_score  #Rounded to match desired format
+    return collection_similarity_vector
+"""
+
+
+def cross_word_question_corpus(matrix_corpus, matrix_question):
+    matrix_dimension_M = []
+    for h in range(len(matrix_question)):
+        for i in range(len(matrix_corpus)):
+            if matrix_question[h][0] == matrix_corpus[i][0]:
+                matrix_dimension_M.append(matrix_corpus[i])
+
+    return matrix_dimension_M
+
+
+"""
+def file_similarity(coll):
+    print(coll)
+    highest_value = max(coll.values())  #Finding the greatest value
+    for file, value in coll.items():
+        if value == highest_value:
+            return file  #Return the corresponding file
+"""
+
+
+def file_clean_to_speach(file):
+    file = file[8:]
+    return file
+
+
+def important_word_question(matrix_question):
+    for i in range(1, len(matrix_question) - 1):
+        if matrix_question[i][1] > matrix_question[i + 1][1]:
+            index_i = i
+        else:
+            index_i = i + 1
+    impactant_word = matrix_question[index_i][0]
+    return impactant_word
+
+
+"""
+def sentence_prompt(file_speech,word):
+    string_punctuation = " -,?:;.'()"
+    phrase_1 = ""
+    phrase_x = ""
+    with open("./speeches/"+file_speech,"r") as f:
+        content = f.read()
+        content = content.replace("\n"," ")
+        for i in range (len(content)):
+            if content[i] != chr(46):
+                phrase_1 = phrase_1 + content[i]
+            else:
+                break
+
+        if word in lower_case(punctuation_str(phrase_1)):
+            if lower_case(punctuation_str(phrase_x + content[i + 1])) not in string_punctuation:
+                return phrase_1.lower() + chr(46)
+        else:
+            for i in range(len(phrase_1),len(content)):
+
+                if content[i] != chr(46):
+                    phrase_x = phrase_x + content[i]
+                else:
+                    if word in lower_case(punctuation_str(phrase_x)) :
+                        if lower_case(punctuation_str(phrase_x + content[i + 1])) not in string_punctuation:
+                            return phrase_x.lower() + chr(46)
+                    else:
+                        phrase_x = ""
+"""
+
+
+# ______________________________Response-PART-II______________________________#
+def display_less_important_words(word_not_important):
+    set_word_not_important = ""
+    for i in range(len(word_not_important)):
+        set_word_not_important = set_word_not_important + word_not_important[i] + "\n"
+    # Create a new top-level window
+    dialog = tk.Toplevel()
+    dialog.title("Words Less Importants")
+    dialog.geometry("1000x300")  # Set your desired size
+
+    # Create a scrolled text widget for displaying information
+    txt = scrolledtext.ScrolledText(dialog, wrap=tk.WORD, width=40, height=10)
+    txt.pack(padx=10, pady=10)
+
+    # Inserting some text (replace this with your actual content)
+    txt.insert(tk.INSERT, set_word_not_important)
+
+    # Disable editing of the text
+    txt.configure(state='disabled')
+
+    # Button to close the dialog
+    btn_close = tk.Button(dialog, text="Close", command=dialog.destroy)
+    btn_close.pack(pady=10)
+
+
+def display_most_important_words(word_import_folder):
+    # Logic for displaying the most important words
+    messagebox.showinfo("Here is the most important word in the directory :\n", word_import_folder)
+
+
+# Variable globale pour stocker le text entré
+question = ""
+
+
+def display_most_important_words_Chirac(word_import_chirac):
+    # Logic for displaying the most important words
+    messagebox.showinfo("more important words", "more important words here :\n" + word_import_chirac)
+
+
+# Variable globale pour stocker le text entré
+
+def display_presi_nation(president_nation, presi_nation):
+    string = ""
+    for i in range(len(president_nation)):
+        string = string + president_nation[i]
+    # Logic for displaying the most important words
+    messagebox.showinfo("Here are the presidents talking about Nation :\n", string, "\n-", presi_nation,
+                        "speaks most of Nation")
+
+
+# Variable globale pour stocker le text entré
+
+def display_presi_ecolo(president_ecolo):
+    # Logic for displaying the most important words
+    messagebox.showinfo("Here's the president who talks most about ecology :\n", president_ecolo)
--- main.py
+++ main.py
@@ -1,1 +1,171 @@
+# 16/12 23h
+from function3 import *
 
+if __name__ == "__main__":
+
+    # Call of the function
+    directory = "./speeches-20231123"
+    files_names = list_of_files(directory, "txt")
+
+    last_list = new_list(files_names)
+    last_column_list = sorted(last_list)
+    president_list = del_duplicates(last_list)
+
+    cleaned(files_names)
+
+    word_collection = {}
+    directory_cleaned = "./cleaned"
+    word_collection = cpt_word(directory_cleaned)
+
+    files_into_speeches = [f for f in os.listdir('./speeches-20231123') if f.endswith('.txt')]
+
+    # Calling cleaning function
+    cleaned(files_into_speeches)
+
+    coll_IDF = IDF_score_coll(directory_cleaned)
+
+    Matrix = TFIDF_matrix(directory_cleaned)
+
+    # To display the Matrix
+    for i in range(len(Matrix)):
+        print(Matrix[i])
+
+    word_not_important = []
+    for j in range(1, len(Matrix)):
+        TF_IDF = Matrix[j][1]
+        if TF_IDF == 0:
+            sum_TF_IDF = 0
+            for k in range(2, 9):
+                sum_TF_IDF = sum_TF_IDF + Matrix[j][k]
+            if sum_TF_IDF == 0:
+                word_not_important.append(Matrix[j][0])
+
+    # Initialise variables to store the word with the highest score
+    highest_score = 0
+    word_import_folder = ""
+
+    # Iteration on each row and column of the Matrix (ignoring the first row and column)
+    for i in range(1, len(Matrix)):
+        for j in range(1, len(Matrix[i])):
+            if Matrix[i][j] > highest_score:
+                highest_score = Matrix[i][j]
+                word_import_folder = Matrix[i][0]
+
+    # Initialise variables to store the word with the highest score
+    highest_score = 0
+    word_import_chirac = ""
+
+    # Iteration on each row and column of the Matrix (ignoring the first row and column)
+    for i in range(1, len(Matrix)):
+        for j in range(1, 3):
+            if Matrix[i][j] > highest_score:
+                highest_score = Matrix[i][j]
+                word_import_chirac = Matrix[i][0]
+
+    president_nation = []
+    index_word_nation = 0
+    for i in range(len(Matrix)):
+        if Matrix[i][0] == "nation":
+            index_word_nation = i
+    find_Chirac = False
+    find_Mitterrand = False
+    for i in range(2, 9):
+        if Matrix[index_word_nation][i] > 0:
+            if i == 1 or i == 2 and find_Chirac == False:
+                find_Chirac = True
+                president_nation.append("Chirac")
+            if i == 3:
+                president_nation.append("Giscard")
+            if i == 4:
+                president_nation.append("Hollande")
+            if i == 5:
+                president_nation.append("Macron")
+            if i == 6 or i == 7 and find_Mitterrand == False:
+                find_Mitterrand = True
+                president_nation.append("Mitterrand")
+            if i == 8:
+                president_nation.append("Sarkozy")
+
+    index_high_IDF = 0
+    for i in range(2, 8):
+        if Matrix[index_word_nation][i] < Matrix[index_word_nation][i + 1]:
+            index_high_IDF = i + 1
+        else:
+            index_high_IDF = i
+
+    if index_high_IDF == 1 or index_high_IDF == 2:
+        presi_nation = "Chirac"
+    if index_high_IDF == 3:
+        presi_nation = "Giscard"
+    if index_high_IDF == 4:
+        presi_nation = "Hollande"
+    if index_high_IDF == 5:
+        presi_nation = "Macron"
+    if index_high_IDF == 6 or index_high_IDF == 7:
+        presi_nation = "Mitterrand"
+    if index_high_IDF == 8:
+        presi_nation = "Sarkozy"
+
+    word_without_not_import = []
+    for i in range(1, len(Matrix)):
+        if Matrix[i][0] not in word_not_important:
+            word_without_not_import.append(Matrix[i][0])
+
+    ecolo_index = 0
+    for i in range(len(Matrix)):
+        if Matrix[i][0] == "climat":
+            ecolo_index = i
+            break
+
+    # Find the highest score for 'climate'.
+    score_max = 0
+    ecolo_index_president = 0
+    for j in range(1, len(Matrix[ecolo_index])):
+        if Matrix[ecolo_index][j] > score_max:
+            score_max = Matrix[ecolo_index][j]
+            ecolo_index_president = j
+
+    president_ecolo = ""
+    if ecolo_index_president == 1 or ecolo_index_president == 2:
+        president_ecolo = "Chirac"
+    if ecolo_index_president == 3:
+        president_ecolo = "Giscard"
+    if ecolo_index_president == 4:
+        president_ecolo = "Hollande"
+    if ecolo_index_president == 5:
+        president_ecolo = "Macron"
+    if ecolo_index_president == 6 or ecolo_index_president == 7:
+        president_ecolo = "Mitterrand"
+    if ecolo_index_president == 8:
+        president_ecolo = "Sarkozy"
+
+    # Main menu loop
+    flag = True
+    while flag:
+        print("\n--- Menu ---\n\n"
+              "- To display the least important words, enter 1\n"
+              "- To display the most important words, enter 2\n"
+              "- To display the most important word by Chirac, enter 3\n"
+              "- To display the presidents who talk about the Nation, as well as the one who talks about it the most, enter 4\n"
+              "- To display the president who speaks most about ecology, enter 5\n"
+              "- Sorry, this command is not available at the moment : (To access the bot, enter 0.)\n")
+        value_menu = saisie()  # Saisie d'un caractère (nombre de 0 à 5)
+        if value_menu == 1:
+            print("\n- Voici les mots les moins importants\n")
+            for i in range(len(word_not_important)):
+                print(word_not_important[i])
+        elif value_menu == 2:
+            print("\n- Voici le mot le plus important du dossier :\n", word_import_folder)
+        elif value_menu == 3:
+            print("\n- Voici le mot le plus important des discours de Chirac : ", word_import_chirac)
+        elif value_menu == 4:
+            print("\n- Voici les présidents parlant de Nation\n")
+            for i in range(len(president_nation)):
+                print(president_nation[i])
+            print("\n-", presi_nation, "parle le plus de Nation")
+        elif value_menu == 5:
+            print("\n-", president_ecolo, "est le président qui parle le plus du climat\n")
+        elif value_menu == 0:
+            flag = False
+        else:
+            print("Choix incorrect, retour au menu")
